52c2a881ce032c876298fd6948bc841a319b0ef8
Guilherme Ferreira <ferreiramoraesg@gmail.com>
2025-10-02 11:47:33 -0300

    feat: Enhance Account retrieval to include user information and improve error handling

--- src/feature/Account/getAccount.js
+++ src/feature/Account/getAccount.js
@@ -1,10 +1,30 @@
-const Account = require("../../models/Account")
+const Account = require("../../models/Account");
 
-const getAccount = async ({
-  filter, repository
-}) => {
-  const result = await repository.get(filter)
-  return result?.map(user => new Account(user))
-}
+const getAccount = async ({ filter, repository, userRepository, userId }) => {
+  const result = await repository.get(filter);
+  const accounts = result?.map((user) => new Account(user));
 
-module.exports = getAccount
+  let userInfo = null;
+  if (userId && userRepository) {
+    try {
+      const user = await userRepository.getById(userId);
+      console.log("Consultando o usuario: ", { user });
+
+      if (user) {
+        userInfo = {
+          username: user.username,
+          email: user.email,
+        };
+      }
+    } catch (error) {
+      console.error("Erro ao buscar informações do usuário:", error.message);
+    }
+  }
+
+  return {
+    accounts,
+    userInfo,
+  };
+};
+
+module.exports = getAccount;
